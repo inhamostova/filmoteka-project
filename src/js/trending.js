27e8f39adf43a23f1bc8362eac3ee8e8be37ac94
inhamostova <inha.mostova@gmail.com>
2026-07-23 23:32:05 +0300

    fixes due to comments

--- src/js/trending.js
+++ src/js/trending.js
@@ -1,12 +1,11 @@
 import { fetchTrendingMovies } from './services/movies-api';
-import { createMovieCardsMarkup } from './render/renderTrending';
+import { createMovieCardsMarkup } from './render/movieCardsMarkup.js';
 
 const gallery = document.querySelector('.js-gallery');
 
 async function loadTrendingMovies() {
   try {
     const data = await fetchTrendingMovies();
-    console.log(data.results);
     gallery.innerHTML = createMovieCardsMarkup(data.results);
   } catch (error) {
     console.error(error.message);
