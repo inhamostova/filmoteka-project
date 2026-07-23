5a01ec3a780dfa08a497c915c2d4fcba3a17fe70
inhamostova <inha.mostova@gmail.com>
2026-07-23 20:32:58 +0300

    fix comment review

--- src/js/services/movies-api.js
+++ src/js/services/movies-api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const BASE_URL = 'https://api.themoviedb.org/3';
-const token =
+const API_TOKEN =
   'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MTdiNGRiMmFiZTcwNGY0Njk1NjdhNmZjZTM4NzA5NyIsIm5iZiI6MTY3MTEzNTI3NC4yNDEsInN1YiI6IjYzOWI4MDJhY2I5ZjRiMDBiMWQ3MDU0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.QzbVo_qbZnszsGvllQ67Bu45o2b6F5lCxocbnK73Lck';
 
 export async function fetchTrendingMovies() {
@@ -10,7 +10,7 @@ export async function fetchTrendingMovies() {
     {
       headers: {
         accept: 'application/json',
-        Authorization: `Bearer ${token}`,
+        Authorization: `Bearer ${API_TOKEN}`,
       },
     }
   );
