2d548a74674ce20414c96749ce68f7982ad9f73e
inhamostova <inha.mostova@gmail.com>
2026-08-02 21:08:42 +0300

    normalize modal styles due to figma

--- src/js/render/createMovieModalMarkup.js
+++ src/js/render/createMovieModalMarkup.js
@@ -1,3 +1,4 @@
+import { sliceNum } from '../helpers/helpers';
 import { makeGenresListInModal } from '../helpers/makeGenresInModal';
 
 const BASE_IMG_URL = 'https://image.tmdb.org/t/p/w500';
@@ -18,15 +19,15 @@ export function createMovieModalMarkup(movie) {
 <div class="modal__left-side"><img class="modal__poster" src="${BASE_IMG_URL}${poster_path}" alt="${original_title}"/></div>
 <div class="modal__right-side">
     <h2 class="modal__title">${title}</h2>
-    <p class="modal__info"><span class="modal__info__data">Vote / Votes</span><span class="modal__info__value">${vote_average} / ${vote_count}</span></p>
-    <p class="modal__info"><span class="modal__info__data popularity">Popularity</span><span class="modal__info__value">${popularity}</span></p>
+<p class="modal__info"><span class="modal__info__data">Vote / Votes</span><span class="modal__info__value"><span class="vote">${sliceNum(vote_average)}</span> / <span class="votes">${vote_count}</span></span></p>
+    <p class="modal__info"><span class="modal__info__data popularity">Popularity</span><span class="modal__info__value">${sliceNum(popularity)}</span></p>
     <p class="modal__info"><span class="modal__info__data">Original Title</span><span class="modal__info__value">${original_title}</span></p>
     <p class="modal__info last"><span class="modal__info__data genre">Genre</span><span class="modal__info__value">${makeGenresListInModal(genres)}</span></p>
     <h3 class="modal__about">About </h3>
     <p class="modal__descr">${overview}</p>
     <ul class="modal-btn-list">
-      <li class="modal-btn-list__item"><button class="modal__btn primary" data-btn="watched" type="button">add to Watched</button></li>
-      <li class="modal-btn-list__item"><button class="modal__btn secondary" data-btn="queue" type="button">add to queue</button></li>
+      <li class="modal-btn-list__item"><button class="modal__btn" data-btn="watched" type="button">add to Watched</button></li>
+      <li class="modal-btn-list__item"><button class="modal__btn" data-btn="queue" type="button">add to queue</button></li>
     </ul>
 </div>`;
 }
